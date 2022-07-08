24e6f52e602fc025fe73997084e91c2685ad93db
Eric Fernandez <efernandezalbuq@gmail.com>
2022-07-07 23:22:40 -0300

    Filtrando cursos comprados
    Acesso apenas a cursos comprados

--- cursos/urls.py
+++ cursos/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', CoursesView.as_view(), name='course'),
     path('<slug:course_slug>', CourseDetailView.as_view(), name='course_details'),
     path('<slug:course_slug>/mural', CourseMuralDetailView.as_view(), name='course_mural'),
-    path('<slug:course_slug>/<slug:lesson_slug>', CourseLessonDetailView.as_view(), name='course_lesson')
+    path('<slug:course_slug>/<slug:lesson_slug>', CourseLessonDetailView.as_view(), name='course_lesson'),
 ]
--- cursos/views.py
+++ cursos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 
 #from .models import User
 from django.contrib.auth.models import User
@@ -12,7 +12,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Course, CourseLesson
+from .models import Course, CourseLesson, BoughtBy
 from .forms import PostForm, LessonPostForm
 
 from django.views.generic.list import ListView
@@ -21,13 +21,55 @@ from django.views.generic.edit import FormMixin
 
 from django.urls import reverse
 
+from frontend.templates.cursos import *
+
 class CoursesView(ListView):
     model = Course
 
 class CourseDetailView(DetailView):
     model = Course
     slug_field = 'course_slug'
-    slug_url_kwarg = 'course_slug'
+    slug_url_kwarg = 'course_slug' 
+
+    def get_context_data(self, **kwargs):
+        user = get_object_or_404(User, username=self.request.user.username)
+        bought = BoughtBy.objects.filter(user_id=user.id)
+        courses = Course.objects.all()
+        this_course = Course.objects.get(course_slug=self.kwargs['course_slug'])
+        print(this_course)
+        user_courses = []
+
+        for course in bought:
+            item = Course.objects.get(pk=course.course_id)
+            user_courses.append(item)
+
+        context = super().get_context_data(**kwargs)
+        context['user'] = user
+        context['courses'] = courses
+        if this_course in user_courses:
+            context['status'] = True 
+        else:
+            context['status'] = False
+        return context
+
+
+def course_access(request, course_slug):
+        print('EXECUTOU')
+        user = User.objects.get(username=request.user.username)
+        bought = BoughtBy.objects.filter(user_id=user.id)
+        courses = []
+        this_course = Course.objects.get(course_slug=course_slug)
+        print(user)
+        print(bought)
+        print(this_course)
+        for course in bought:
+            item = Course.objects.get(pk=course.course_id)
+            courses.append(item)
+        if this_course in courses:
+            print('True')
+            return render(request, 'course_detail.html', {'status': True})
+
+
 
 class CourseMuralDetailView(FormMixin, DetailView):
     model = Course
